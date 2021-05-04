c00b5f0bd0ae7a0cf11194f0c1f213e380822a1c
Davi Uchoa Rodrigues <daviuchoa38@gmail.com>
2021-05-03 23:09:30 -0300

    test: adds the relative path actions dir success case

--- tests/Feature/AdrCrudCommandTest.php
+++ tests/Feature/AdrCrudCommandTest.php
@@ -31,6 +31,22 @@ test('test success', function () {
     }
 });
 
+test('test relative actions dir path success', function () {
+    chdir('/tmp');
+
+    $this->artisan("make:crud {$this->model_name} --actions-dir=./app/actions")
+        ->expectsOutput('User ADR folders and classes created successfuly.')
+        ->assertExitCode(0);
+
+    $actions = ['Index', 'Create', 'Store', 'Edit', 'Update', 'Delete'];
+    foreach ($actions as $action) {
+        assertFilesAreCreated(
+            "{$this->model_dir}/{$action}/{$this->class_name}{$action}Action.php",
+            "{$this->model_dir}/{$action}/{$this->class_name}{$action}Responder.php"
+        );
+    }
+});
+
 test('don\'t overwirte existent files', function () {
     $filesystem = new Filesystem();
     $filesystem->ensureDirectoryExists("{$this->model_dir}/Index");
